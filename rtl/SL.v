33757dd9a9f4b5a7b0f1612a092ba847da0765a5
Lerk-git <e.v.vlasov@gmail.com>
2017-10-16 18:45:50 +0300

    Rewrited FSM in 3-always style

--- rtl/SL.v
+++ rtl/SL.v
@@ -32,6 +32,19 @@ parameter DATA_ADDRESS_WR = 0'b0010;
 parameter DATA_ADDRESS_R  = 0'b0100;
 parameter STATUS_ADDRESS  = 0'b1000;
 
+parameter S0 = 5'b0_0000,
+          S1 = 5'b0_0001,
+          S2 = 5'b0_0010,
+          S3 = 5'b0_0011,
+          S4 = 5'b0_0100,
+          S5 = 5'b0_0101,
+          S6 = 5'b0_0110,
+          S7 = 5'b0_0111,
+          S8 = 5'b0_1000,
+          S9 = 5'b0_1001;
+
+reg [ 4:0] state_r, next_r; //RECEIVER MODE: 0 - idle, 1 - receiveing started, 1a - bit detected, 1b - no bit, 1c - stop bit
+                    //TRANSMITTER MODE: 0 - idle, 1 - sending a word
 
 
 
@@ -55,12 +68,12 @@ reg [15:0] sl1_tmp_r;
 reg [31:0] shift_data_r;
 reg [ 5:0] cycle_cnt_r;
 reg [ 5:0] bit_cnt_r;
-reg [ 4:0] state_r; //RECEIVER MODE: 0 - idle, 1 - receiveing started, 1a - bit detected, 1b - no bit, 1c - stop bit
-                    //TRANSMITTER MODE: 0 - idle, 1 - sending a word
+
 
 reg [31:0] buffered_data_r;
 reg [31:0] data_to_send_r;
 reg [15:0] config_r; //[0] - parity check, [6:1] - bit cnt(8-32bits), [7] - rxtx mode, [8] - IRQ mode ####address on config_r is 0 (zero)
+parameter PCE = 0;
 reg [15:0] status_r; //[0] - word length fail, [1] - word receiveing process going, [2] - empty
                     //[3] - word received, [4] - parity error, [5] - level error on line,
 reg parity_ones;
@@ -87,122 +100,162 @@ reg [15:0] sync2_config_r;
 
 reg [2:0] apb_state; //001 - IDLE, 010 - SETUP, 100 - ACCESS
 
-
+wire bit_ended;
+wire bit_started;
 
 
 assign pdata_a = ( penable_a && psel_a && !pwrite_a ) ? apb_muxed_out_r : 32'bz;
-
-
+assign bit_ended   = (sl0_tmp_r[7:0] == 8'hFF && sl1_tmp_r[7:0] == 8'hFF) ? 1 : 0;
+assign bit_started = (sl0_tmp_r[15:12] == 4'hF && sl0_tmp_r[3:0] == 4'h0) || (sl1_tmp_r[15:12] == 4'hF && sl1_tmp_r[3:0] == 4'h0) ? 1 : 0;
 
 
 
 
 always @(posedge clk or negedge rst_n or negedge preset_n_a) begin
-if( !rst_n | !preset_n_a ) begin
-  sl0_tmp_r[15:0]       <= 16'hAAAA;
-  sl1_tmp_r[15:0]       <= 16'hAAAA;
-  shift_data_r[31:0]    <= 0;
-  cycle_cnt_r[5:0]      <= 0;
-  bit_cnt_r[5:0]        <= 0;
-  state_r[4:0]          <= 5'b00001;
-  buffered_data_r[31:0] <= 0;
-  data_to_send_r[31:0]  <= 0;
-  config_r[15:0]        <= 16'h0020;
-  status_r[15:0]        <= 0;
-  parity_zeroes         <= 0;
-  parity_ones           <= 1;
-end else  if( !config_r[7] )  //RECEIVER ROUTINE
-  begin
-    sl0_tmp_r[15:0] <= ( sl0_tmp_r << 1 ) | serial_line_zeroes_a ;
-    sl1_tmp_r[15:0] <= ( sl1_tmp_r << 1 ) | serial_line_ones_a;
-    case (state_r)
-      5'b00001: begin //IDLE state, waiting for active transmission sequence on one of input lines
-                  if( (sl0_tmp_r[15:12] == 4'hF && sl0_tmp_r[3:0] == 4'h0) || (sl1_tmp_r[15:12] == 4'hF && sl1_tmp_r[3:0] == 4'h0) )
-                  begin
-                    state_r <= state_r << 1;
-                    cycle_cnt_r <= 3;
-                    status_r[0] <= 0; //word length ok
-                    status_r[1] <= 1; //receiving process on
-                    status_r[3] <= 0; //Word received flag off
-                    status_r[4] <= 0; //no parity error atm
-                    status_r[5] <= 0; //no level errors on line
-                  end
-                  else begin
-                    status_r[0] <= 0; //word length ok
-                    status_r[1] <= 0; //receiving process off
-                    status_r[3] <= 0; //Word received flag off
-                    status_r[4] <= 0; //no parity error atm
-                    status_r[5] <= 0; //no level errors on line
-                  end
-                end
-      5'b00010: begin //RECEIVING state
-                  if( cycle_cnt_r == STROB_POS )
-                  begin
-                    state_r <= state_r << 1;
-                    if ( !serial_line_ones_a && !serial_line_zeroes_a ) //Если стоп-бит
-                    begin
-                      parity_zeroes <= 0;
-                      parity_ones   <= 1;
-                      shift_data_r  <= 0;
-                      bit_cnt_r     <= 0;
-                      if( bit_cnt_r[5:0] == config_r[6:1] ) //Проверяем количество принятых разрядов
-                      begin
-                        status_r[0] <= 0; //word length ok
-                        status_r[1] <= 0; //receiving process ended
-                        status_r[2] <= 0; //as word correctly received, no noise on inout lines
-                        status_r[3] <= 1; //word received
-
-                        if( (!parity_zeroes || !parity_ones ) && config_r[0] )  buffered_data_r <= 0;
-                        else  buffered_data_r <= shift_data_r;
-                      end
-                      else begin
-                        status_r[0] <= 1; //word length fail
-                        status_r[1] <= 0; //receiving process ended
-                        status_r[2] <= 0; //as word correctly received, no noise on inout lines
-                        status_r[3] <= 0; //word is not received
-                      end
-
-                      if( !parity_zeroes && !parity_ones ) status_r[4] <= 0; //parity ok
-                      else status_r[4] <= 1; //parity check fail
-                    end
-                    else
-                    begin
-                      if( bit_cnt_r[5:0] < config_r[6:1] ) begin  //not parity bit
-                        if ( !serial_line_ones_a ) //Если единичка
-                          begin
-                            shift_data_r <= ( shift_data_r >> 1 ) | ( 1 << config_r[6:1] - 1 ); //Store data in high bits of register
-                            parity_ones  <= parity_ones ^ 1;
-                            bit_cnt_r <= bit_cnt_r + 1;
-                          end
-                          else//Если нолик
-                            begin
-                              shift_data_r  <= ( shift_data_r >> 1 ) & ~( 1 << config_r[6:1] -1 );
-                              parity_zeroes <= parity_zeroes ^ 1;
-                              bit_cnt_r <= bit_cnt_r + 1;
-                            end
-                      end else
-                        if ( !serial_line_ones_a ) parity_ones  <= parity_ones ^ 1;
-                          else  parity_zeroes <= parity_zeroes ^ 1;
-                    end
-                  end
-                  else cycle_cnt_r <= cycle_cnt_r + 1;
-                end
-      5'b00100: begin //WAITING for bit transmission end state
-                  if( sl0_tmp_r[7:0] == 8'hFF && sl1_tmp_r[7:0] == 8'hFF )
-                  begin
-                    state_r <= state_r >> 2; //go to the IDLE state
-                    cycle_cnt_r <= 0;
-                  end
-                  else cycle_cnt_r <= cycle_cnt_r + 1;
-                end
-      default: state_r <= 5'b00001;
-    endcase
-  end
-  else begin //TRANSMITTER routine
-    state_r <= 5'b00001;
-  end
-
+  if( !rst_n || !preset_n_a ) state_r <= S0;
+  else  state_r <= next_r;
 end
+
+
+
+always @(state_r or serial_line_ones_a or serial_line_zeroes_a or cycle_cnt_r or bit_cnt_r or bit_ended or parity_ones or parity_zeroes or config_r[PCE]) begin
+  next_r <= 'bx;
+  case(state_r)
+    S0: if( bit_started && bit_cnt_r[5:0] == 6'b00_0000 )                                    next_r = S1; //S1 - wait state w/ flush
+        else if( bit_started )                                                              next_r = S2; //S0 - wait state w/o flush
+        else                                                                                next_r = S0;
+    S1:                                                                                     next_r = S2;
+    S2: if( cycle_cnt_r < STROB_POS )                                                       next_r = S2; //
+        else if( !serial_line_ones_a && !serial_line_zeroes_a && cycle_cnt_r == STROB_POS ) next_r = S3; //Go to STOP bit routine
+        else if( !serial_line_ones_a &&  serial_line_zeroes_a && cycle_cnt_r == STROB_POS ) next_r = S4; //Go to ONE bit routine
+        else if(  serial_line_ones_a && !serial_line_zeroes_a && cycle_cnt_r == STROB_POS ) next_r = S5; //Go to ZERO bit routine
+        else                                                                                next_r = S9; //Error condition, go to S9
+    S3: if( bit_cnt_r[5:0] == config_r[6:1] && (!config_r[PCE] | !(parity_ones | parity_zeroes)) )    next_r = S6; //Go to data ok routine
+        else if( bit_cnt_r[5:0] == config_r[6:1] && config_r[PCE] &&  (parity_ones | parity_zeroes) ) next_r = S7;//Go to PEF routine
+        else                                                                                          next_r = S8; //Go to WLF routine
+    S4: if( bit_ended )  next_r = S0; //Go to waiting state
+        else             next_r = S4;
+    S5: if( bit_ended )  next_r = S0; //Go to waiting state
+        else             next_r = S5;
+    S6: if( bit_ended )  next_r = S0; //Go to waiting state
+        else             next_r = S6;
+    S7: if( bit_ended )  next_r = S0; //Go to waiting state
+        else             next_r = S7;
+    S8: if( bit_ended )  next_r = S0; //Go to waiting state
+        else             next_r = S8;
+    S9: if( bit_ended )  next_r = S0; //Go to waiting state
+        else             next_r = S9;
+    default:             next_r = 'bx;
+  endcase
+end
+
+
+always @(posedge clk or negedge rst_n or negedge preset_n_a) begin
+  if( !rst_n | !preset_n_a ) begin
+    sl0_tmp_r[15:0]       <= 16'hAAAA;
+    sl1_tmp_r[15:0]       <= 16'hAAAA;
+    shift_data_r[31:0]    <= 0;
+    cycle_cnt_r[5:0]      <= 0;
+    bit_cnt_r[5:0]        <= 0;
+    buffered_data_r[31:0] <= 0;
+    data_to_send_r[31:0]  <= 0;
+    config_r[15:0]        <= 16'h0020;
+    status_r[15:0]        <= 0;
+    parity_zeroes         <= 0;
+    parity_ones           <= 1;
+  end else begin
+      sl0_tmp_r[15:0] <= ( sl0_tmp_r << 1 ) | serial_line_zeroes_a ;
+      sl1_tmp_r[15:0] <= ( sl1_tmp_r << 1 ) | serial_line_ones_a;
+
+      case(next_r)
+        S0: begin
+              cycle_cnt_r <= 0;
+            end
+        S1: begin //wait state with flush
+              status_r[15:0] <= 0;
+            end
+        S2: begin
+              cycle_cnt_r <= cycle_cnt_r + 1;
+              status_r[0] <= 0; //word length ok
+              status_r[1] <= 1; //receiving process on
+              status_r[3] <= 0; //Word received flag off
+              status_r[4] <= 0; //no parity error atm
+              status_r[5] <= 0; //no level errors on line
+            end
+        S3: begin
+
+            end
+        S4: begin
+              shift_data_r <= ( shift_data_r >> 1 ) | ( 1 << config_r[6:1] - 1 ); //Store data in high bits of register
+              parity_ones  <= parity_ones ^ 1;
+              bit_cnt_r    <= bit_cnt_r + 1;
+            end
+        S5: begin
+              shift_data_r  <= ( shift_data_r >> 1 ) & ~( 1 << config_r[6:1] - 1 );
+              parity_zeroes <= parity_zeroes ^ 1;
+              bit_cnt_r     <= bit_cnt_r + 1;
+            end
+        S6: begin //DATA recevied correctly
+              parity_zeroes <= 0;
+              parity_ones   <= 1;
+              shift_data_r  <= 0;
+              bit_cnt_r     <= 0;
+              cycle_cnt_r   <= 0;
+              status_r[0]   <= 0; //word length ok
+              status_r[1]   <= 0; //receiving process ended
+              status_r[2]   <= 0; //as word correctly received, no noise on inout lines
+              status_r[3]   <= 1; //word received
+              status_r[4]   <= 0; //Dont care about parity value
+              status_r[5]   <= 0; //no level errors on line
+              buffered_data_r <= shift_data_r;
+            end
+        S7: begin
+              parity_zeroes <= 0;
+              parity_ones   <= 1;
+              shift_data_r  <= 0;
+              bit_cnt_r     <= 0;
+              cycle_cnt_r   <= 0;
+              status_r[0]   <= 0; //word length ok
+              status_r[1]   <= 0; //receiving process ended
+              status_r[2]   <= 0; //as word correctly received, no noise on inout lines
+              status_r[3]   <= 1; //word received
+              status_r[4]   <= 1; //Parity error detected
+              status_r[5]   <= 0; //no level errors on line
+              buffered_data_r <= 32'h0000_0000;
+            end
+        S8: begin
+              parity_zeroes <= 0;
+              parity_ones   <= 1;
+              shift_data_r  <= 0;
+              bit_cnt_r     <= 0;
+              cycle_cnt_r   <= 0;
+              status_r[0]   <= 1; //word length check error
+              status_r[1]   <= 0; //receiving process ended
+              status_r[2]   <= 0; //as word correctly received, no noise on inout lines
+              status_r[3]   <= 1; //word received
+              status_r[4]   <= 0; //Parity error detected
+              status_r[5]   <= 0; //no level errors on line
+              buffered_data_r <= 32'h0000_0000;
+            end
+        S9: begin
+              parity_zeroes <= 0;
+              parity_ones   <= 1;
+              shift_data_r  <= 0;
+              bit_cnt_r     <= 0;
+              cycle_cnt_r   <= 0;
+              status_r[0]   <= 0; //word length check error
+              status_r[1]   <= 0; //receiving process ended
+              status_r[2]   <= 0; //as word correctly received, no noise on inout lines
+              status_r[3]   <= 0; //word received
+              status_r[4]   <= 0; //Parity error detected
+              status_r[5]   <= 1; // level errors on line
+              buffered_data_r <= 32'h0000_0000;
+            end
+      endcase
+    end
+end
+
+
 
 always @(posedge pclk_a or negedge rst_n or negedge preset_n_a) begin
   if ( !rst_n || !preset_n_a ) begin
@@ -226,61 +279,44 @@ always @(posedge pclk_a or negedge rst_n or negedge preset_n_a) begin
     apb_config_r          <= sync1_config_r;
     apb_status_r          <= sync1_status_r;
 
-    // case( apb_state )
-    // 3'b001: begin //IDLE state, PSELx = 0, PENABLE = 0
-              if( psel_a && !penable_a ) begin
-                // apb_state <= 3'b010;
+    if( psel_a && !penable_a ) begin
+      if( pwrite_a ) begin // WRITE sequence
+        case( paddr_a )
+          CONFIG_ADDRESS:
+            begin
+              config_r[15:0] <= in_pdata_r[15:0];
+            end
+          DATA_ADDRESS_WR:
+            begin
+              data_to_send_r[31:0] <= in_pdata_r[31:0];
+            end
+          default:
+            begin
 
-                if( pwrite_a ) begin // WRITE sequence
-                  case( paddr_a )
-                    CONFIG_ADDRESS:
-                      begin
-                        config_r[15:0] <= in_pdata_r[15:0];
-                      end
-                    DATA_ADDRESS_WR:
-                      begin
-                        data_to_send_r[31:0] <= in_pdata_r[31:0];
-                      end
-                    default:
-                      begin
-
-                      end
-                  endcase
-                end
-                else begin //READ sequence
-                  case( paddr_a )
-                    CONFIG_ADDRESS:
-                      begin
-                        apb_muxed_out_r[31:0] <= { 16'h0000, apb_config_r[15:0] };
-                      end
-                    DATA_ADDRESS_R:
-                      begin
-                        apb_muxed_out_r[31:0] <= apb_buffered_data_r[31:0];
-                      end
-                    STATUS_ADDRESS:
-                      begin
-                        apb_muxed_out_r[31:0] <= { 16'h0000, apb_status_r[15:0] };
-                      end
-                    default:
-                      begin
-                        apb_muxed_out_r[31:0] <= 32'h0000_0000;
-                      end
-                  endcase
-                end
-              end
-
-    //           end else apb_state <= 3'b001;
-    //         end
-    // 3'b010: begin //SETUP state, PSELx = 1, PENABLE = 0
-    //           apb_state <= 3'b001;
-
-    //         end
-    // 3'b100: begin //ACCESS state, PSELx = 1, PENABLE = 1
-    //           apb_state <= 3'b001;
-    //         end
-    // default: apb_state <= 3'b001;
-
-    // endcase
+            end
+        endcase
+      end
+      else begin //READ sequence
+        case( paddr_a )
+          CONFIG_ADDRESS:
+            begin
+              apb_muxed_out_r[31:0] <= { 16'h0000, apb_config_r[15:0] };
+            end
+          DATA_ADDRESS_R:
+            begin
+              apb_muxed_out_r[31:0] <= apb_buffered_data_r[31:0];
+            end
+          STATUS_ADDRESS:
+            begin
+              apb_muxed_out_r[31:0] <= { 16'h0000, apb_status_r[15:0] };
+            end
+          default:
+            begin
+              apb_muxed_out_r[31:0] <= 32'h0000_0000;
+            end
+        endcase
+      end
+    end
 
   end
 end
