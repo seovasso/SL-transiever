9d4e13ae0e20629d0233ec0e65b783c0817d3395
seovasso <seo-vasso@yandex.ru>
2017-11-17 17:30:28 +0300

    Some variables names fixed

--- bench/hdl/AsincFifoTb.sv
+++ bench/hdl/AsincFifoTb.sv
@@ -1,12 +1,12 @@
 `timescale 100ps / 1ps
 
 module AsincFifoTb();
-  parameter clkPeriod   = 10;
-  parameter clkTimeDiff = 3;
-  parameter dataSize = 32;
+  parameter CLK_PERIOD   = 10;
+  parameter CLK_PERIOD = 3;
+  parameter FIFO_DATA_SIZE = 32;
   parameter addrSize = 3;
 
-  logic [dataSize-1:0] rd_data, wr_data;
+  logic [FIFO_DATA_SIZE-1:0] rd_data, wr_data;
   logic                wr_full,
                        rd_empty,
                        wr_inc,
@@ -16,7 +16,7 @@ module AsincFifoTb();
                        rd_rst_n,
                        rd_clk;
 
-  AsyncFifo#(addrSize,dataSize) fifo1 ( .rd_data  (rd_data),
+  AsyncFifo#(addrSize,FIFO_DATA_SIZE) fifo1 ( .rd_data  (rd_data),
                                         .wr_data  (wr_data),
                                         .wr_full  (wr_full),
                                         .wr_inc   (wr_inc),
@@ -27,40 +27,40 @@ module AsincFifoTb();
                                         .wr_rst_n (wr_rst_n),
                                         .rd_rst_n (rd_rst_n));
 task writeToBuffer;
-  input logic [dataSize-1:0] dataToSend;
+  input logic [FIFO_DATA_SIZE-1:0] dataToSend;
   begin
-    #(clkTimeDiff);
+    #(CLK_PERIOD);
     wr_data = dataToSend;
     if (!wr_full) wr_inc = 1;
     else $display("Write operation aborted: buffer is full");
-    #clkPeriod;
+    #CLK_PERIOD;
     wr_inc = 0;
     wr_data = 0;
-    #(clkPeriod-clkTimeDiff);
+    #(CLK_PERIOD-CLK_PERIOD);
   end
 endtask
-logic [dataSize-1:0] readedData;
+logic [FIFO_DATA_SIZE-1:0] readedData;
 task readFromBuffer;
   begin
     if (!rd_empty) begin
       readedData = rd_data;
       rd_inc = 1;
-      #clkPeriod;
+      #CLK_PERIOD;
       rd_inc = 0;
-      #clkPeriod;
+      #CLK_PERIOD;
     end
     else $display("Read operation aborted: buffer is empty");
     rd_inc = 0;
   end
 endtask
   initial begin
-     #(clkTimeDiff);
-      forever #(clkPeriod/2) wr_clk <= ~ wr_clk;
+     #(CLK_PERIOD);
+      forever #(CLK_PERIOD/2) wr_clk <= ~ wr_clk;
     end
-  initial forever #(clkPeriod/2) rd_clk <= ~ rd_clk;
+  initial forever #(CLK_PERIOD/2) rd_clk <= ~ rd_clk;
 
 
-  logic [dataSize-1:0] testDataArr [(1 << addrSize)-1:0];
+  logic [FIFO_DATA_SIZE-1:0] testDataArr [(1 << addrSize)-1:0];
   logic currTestPassed;
   logic allTestsPassed;
   initial begin
@@ -74,12 +74,12 @@ endtask
     rd_inc = 0;
     wr_rst_n = 1;
     rd_rst_n = 1;
-    #(clkPeriod*5) wr_rst_n=0;
+    #(CLK_PERIOD*5) wr_rst_n=0;
     rd_rst_n = 0;
-    #(clkPeriod*5) wr_rst_n=1;
+    #(CLK_PERIOD*5) wr_rst_n=1;
     rd_rst_n=1;
   //Test 1;
-    #(clkPeriod*2);
+    #(CLK_PERIOD*2);
     if (!rd_empty) begin
     currTestPassed = 0;
     allTestsPassed = 0;
@@ -88,11 +88,11 @@ endtask
     // Test 2
     currTestPassed = 1;
     for (int i=0;  i < (1<<addrSize); i++) begin
-      testDataArr [i] = $urandom_range((1<<dataSize-1),0);
+      testDataArr [i] = $urandom_range((1<<FIFO_DATA_SIZE-1),0);
       writeToBuffer( testDataArr [i] );
     end
 
-    #(clkPeriod);
+    #(CLK_PERIOD);
     if (!wr_full) begin
     currTestPassed = 0;
     allTestsPassed = 0;
