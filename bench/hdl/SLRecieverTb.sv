be2ca14b3ae50c24706ebfa52c1d3d273e33310b
seovasso <seo-vasso@yandex.ru>
2018-01-10 13:22:55 +0300

    reworked a little

--- bench/hdl/SLRecieverTb.sv
+++ bench/hdl/SLRecieverTb.sv
@@ -1,8 +1,8 @@
 `timescale 100ps / 1ps
 
 module SlRecieverTb();
-    parameter SlClkLength=32;
-    parameter clkPeriod=2;
+    int    SlClkLength = 32;
+    parameter clkPeriod = 2;
      logic        rst_n;
      logic        clk;
      logic        inSl0;
@@ -13,6 +13,36 @@ module SlRecieverTb();
      logic [31:0] data_w;
      logic [15:0] status_w;
      logic data_status_changed;
+
+ SL_receiver res (
+     .rst_n                      (rst_n),
+     .serial_line_zeroes_a       (inSl0),
+     .serial_line_ones_a         (inSl1),
+     .r_config_w                 (r_config_w),
+     .data_w                     (data_w),
+     .wr_config_w                (wr_config_w),
+     .status_w                   (status_w),
+     .clk                        (clk),
+     .wr_enable                  (wr_enable),
+     .data_status_changed(data_status_changed)
+ );
+
+ logic currTestPassed,allTestsPassed;
+task writeTestResult;
+ input logic condition;
+ input int testNumber;
+ input string testName;
+   begin
+   if (!condition) begin
+     currTestPassed = 0;
+     allTestsPassed = 0;
+   end
+   $display ("Test # %d: %s : %s ",testNumber, testName, (currTestPassed? "OK" : "failed"));
+   currTestPassed = 1;
+ end
+endtask
+
+
 task testMassage;//конфигуриррует приемник и отправляет SL посылку
          input int mesLength;//длинна сообщения
          input int confLength;//сконфигурированая длинна
@@ -77,29 +107,16 @@ task slTransaction;
      #(SlClkLength/2);
      end
 endtask
-
-    SL_receiver res (
-        .rst_n                      (rst_n),
-        .serial_line_zeroes_a       (inSl0),
-        .serial_line_ones_a         (inSl1),
-        .r_config_w                 (r_config_w),
-        .data_w                     (data_w),
-        .wr_config_w                (wr_config_w),
-        .status_w                   (status_w),
-        .clk                        (clk),
-        .wr_enable                  (wr_enable),
-        .data_status_changed(data_status_changed)
-    );
  bit [31:0] mes;
  bit [31:0] lastCorrMess;
  initial forever #(clkPeriod/2)clk=~clk;
- logic curTest,allTest;
+
     initial begin
         clk=0;
-        curTest=0;
-        allTest=1;
-        wr_enable=0;
-        wr_config_w=16'h0014;
+        currTestPassed = 0;
+        allTestsPassed = 1;
+        wr_enable = 0;
+        wr_config_w = 16'h0014;
         inSl0 = 1;
         inSl1 = 1;
         rst_n = 1;
@@ -110,53 +127,20 @@ endtask
         #100
         for (int i=1;i<=3;i=i+1)begin
           int l = 8+$urandom_range(1,16)*2;
-          $display("Test #1.%d.1: 1 correct message width PCE=0 l=%d",i,l);
+          $display("Test #%d: 1 correct message width PCE=0 l=%d",i,l);
           testMassage(l,l,0,1);
-          if (mes == data_w && status_w==16'b1000 )begin
-            curTest=1;
-          end else  begin
-            curTest=0;
-            allTest=0;
-          end
-
-          if(curTest) begin
-            $display("test passed");
-          end else begin
-            $display("test failed");
-          end
+          writeTestResult((mes == data_w && status_w==16'b1000), 1, "");
 
           $display("Test #1.%d.2 : 1 correct message with PCE=1 l=%d",i,l);
           testMassage(l,l,1,1);
+          writeTestResult((mes == data_w && status_w==16'b1000), 1, "");
 
-          if (mes == data_w && status_w==16'b1000 )begin
-            curTest=1;
-          end else  begin
-            curTest=0;
-            allTest=0;
-          end
-
-          if(curTest) begin
-            $display("test passed");
-          end else begin
-            $display("test failed");
-          end
 
         //  int messCount = $urandom_range(4,10);
           $display("Test #2.%d:2.2 : %d correct message with PCE=1 l=%d",i,5,l);
           for (int iterator = 0; iterator < 5; iterator++)
               testMassage(l,l,1,1);
-          if (mes == data_w && status_w==16'b1000 )begin
-            curTest=1;
-          end else  begin
-            curTest=0;
-            allTest=0;
-          end
-          if(curTest) begin
-            $display("test passed");
-          end else begin
-            $display("test failed");
-          end
-
+          writeTestResult((mes == data_w && status_w==16'b1000), 1, "");
 
         $display("Test 3.%d:2 : 5 correct message then 1 incorrect then 5 correct  l=%d",i,l);
         for (int iterator = 0; iterator < 5; iterator++)
@@ -165,106 +149,40 @@ endtask
             testMassage(l+2,l,1,1);//некорректное
             for (int iterator = 0; iterator < 5; iterator++)
                 testMassage(l,l,1,1);
-        if (mes == data_w && status_w==16'b1000 )begin
-          curTest=1;
-        end else  begin
-          curTest=0;
-          allTest=0;
-        end
-        if(curTest) begin
-          $display("test passed");
-        end else begin
-          $display("test failed");
-        end
+        writeTestResult((mes == data_w && status_w==16'b1000), 1, "");
 
         $display("Test 4.%d:2 : 5 incorrect message  l=%d",i,l);
 
         for (int iterator = 0; iterator < 5; iterator++) begin
         testMassage(l+$urandom_range(1,3)*2,l,1,1);//некорректное
         end
-        if (status_w==16'b1001 )begin
-          curTest=1;
-        end else  begin
-          curTest=0;
-          allTest=0;
-        end
-        if(curTest) begin
-          $display("test passed");
-        end else begin
-          $display("test failed");
-        end
+        writeTestResult((status_w==16'b1001), 1, "");
 
 
       $display("Test 5.a.2. %d   incorrect message L>N  l=%d",i,l);
 
       testMassage(l+$urandom_range(1,3)*2,l,1,1);//некорректное сообщение
-      if (status_w == 16'b1001) begin
-        curTest=1;
-      end else  begin
-        curTest=0;
-        allTest=0;
-      end
-      if(curTest) begin
-        $display("test passed");
-      end else begin
-        $display("test failed");
-      end
+      writeTestResult((status_w==16'b1001), 0, "");
 
 
     $display( "Test 5.a.1. %d  : incorrect message L<N  l=%d", i, l );
     testMassage(l-$urandom_range(1,3)*2,l,1,1);//некорректное сообщение
-    if (status_w==16'b1001 )begin
-      curTest=1;
-    end else  begin
-      curTest=0;
-      allTest=0;
-    end
-    if(curTest) begin
-      $display("test passed");
-    end else begin
-      $display("test failed");
-    end
+    writeTestResult((status_w==16'b1001), 0, "");
 
     $display( "Test 5.b.1. %d  :  message with parity error l=%d PCE=1", i, l );
     testMassage(l,l,1,1);//корректное сообщение
     lastCorrMess=mes;
     testMassage(l,l,1,0);//некорректное сообщение
-    if (status_w==16'b11000 && lastCorrMess == 0)begin
-      curTest=1;
-    end else  begin
-      curTest=0;
-      allTest=0;
-    end
-    if(curTest) begin
-      $display("test passed");
-    end else begin
-      $display("test failed");
-    end
+    writeTestResult((status_w==16'b11000 && lastCorrMess == 0), 0, "");
 
     $display( "Test 5.b.2. %d  :  message with parity error l=%d PCE=0", i, l );
     testMassage(l,l,0,0);//некорректное сообщение
-    if (status_w==16'b01000 && mes == data_w)begin
-      curTest=1;
-    end else  begin
-      curTest=0;
-      allTest=0;
-    end
-    if(curTest) begin
-      $display("test passed");
-    end else begin
-      $display("test failed");
-    end
-  end
+    writeTestResult((status_w==16'b01000 && mes == data_w), 0, "");
 
-      if(allTest) begin
+      if(allTestsPassed) begin
         $display("All test passed");
        end else $display("Some tests failed");
        $stop;
-    end
-    always @status_w begin
-      if (!data_status_changed && status_w!=0) begin
-          $display("data_status_changed test failed");
-          allTest = 0;
       end
     end
 
